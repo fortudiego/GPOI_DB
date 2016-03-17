e1a9b814ae24c2dcbf1159c0c40e3a53175d2899
Mohamed Ouad <14simo@gmail.com>
2016-03-17 21:28:33 +0100

    Miglioramenti 17/03 - non completo

--- progetto.php
+++ progetto.php
@@ -1,27 +1,12 @@
 <?php
-	
-	require("core/config.php");
-	
-      
+   require("core/config.php");
+   require("core/gestione.php");
+    
+   
+   $ogg = new Gestione();
+   
 
-      if(!isset($_COOKIE['user'])){
-         
-         $sql = "INSERT INTO `gpoi_work`.`Progetto` (`Id_Progetto`, `Titolo`) VALUES (NULL, '');";
-
-          if ($conn->query($sql) === TRUE) {
-             $last_id = $conn->insert_id;
-              setcookie("user", $last_id, time()+(604800)*54); //cookie che scade dopo un anno
-            } 
-         else {
-             echo "ERRORE: <br>" . $conn->error;
-         }
-      }
-
-	 
-	
 ?>
-
-
 <html>
    <head>
       <title>
@@ -38,77 +23,45 @@
          </tr>
       </table>
       <br><br>
-	  
-	  <center><form action="progetto.php" method="post">
-		<input name="add_task" type="submit" value="Aggiungi task" />
-		<input name="end_task" type="submit" value="Termina" /><br><br>
-	 
-	 
-      <table align=center border=2>
-         <tr>
-            <td>
-               Numerazione
-            </td>
-            <td>
-               Nome
-            </td>
-            <td>
-               Durata
-            </td>
-            <td>
-               Partenza
-            </td>
-            <td>
-               Predecessori
-            </td>
-         </tr>
-         
-		 <?php
-			if(isset($_COOKIE['user'])){
+      <center>
+         <form action="progetto.php" method="post">
+            <input name="btn_add" type="submit" value="Aggiungi task" />
+            <input name="end_task" type="submit" value="Termina" /><br><br>
+            <table align=center border=2>
+               <tr>
+                  <td>
+                     Numerazione
+                  </td>
+                  <td>
+                     Nome
+                  </td>
+                  <td>
+                     Durata
+                  </td>
+                  <td>
+                     Partenza
+                  </td>
+                  <td>
+                     Predecessori
+                  </td>
+               </tr>
+              <?php
+                if(!isset($_COOKIE['user'])){ 
+                  $ogg->setta_cookie();
+                }
 
-            $utente = $_COOKIE['user'];
-				$query_valori = "SELECT * FROM Task WHERE Id_Progetto_E = ".$utente;
-            $risultato_estrai = $conn->query($query_valori);
-            
-            if ($risultato_estrai) {
+              ?>
 
-               $righe = mysqli_num_rows($risultato_estrai);
-               //for($i=0; $i<$righe; $i++){
-                  
-                   while($row = mysqli_fetch_array($risultato_estrai, MYSQLI_NUM)){
-                     
-                  
-                     echo "
-                      <tr>
-                        <td>
-                           <input  name=\"numerazione\" />
-                        </td>
-                        <td>
-                           <input type=\"text\" name=\"nome\" />
-                        </td>
-                        <td>
-                           <input type=\"text\" name=\"durata\" />
-                        </td>
-                        <td>
-                           <input type=\"text\" name=\"earlystart\" />
-                        </td>
-                        <td>
-                           <input type=\"text\" name=\"latestart\" />
-                        </td>
-                      </tr> ";
-               //}
-               }
-               
-            } 
-            else {
-             die ("ERRORE: <br>" . $conn->error);
-            }
-				 
-				
-			}
-		 ?>
-         
-      </table>
-	  </form></center>
+              <?php
+
+                if(isset($_POST['btn_add'])){
+                  $ogg->inserisci(); 
+                }
+                $ogg->stampa();
+              ?>
+             
+            </table>
+         </form>
+      </center>
    </body>
 </html>
