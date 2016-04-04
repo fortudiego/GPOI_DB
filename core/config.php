3c7ace15beca2bc0cac5486b7b395ae4e994cdb6
Mohamed Ouad <14simo@gmail.com>
2016-04-04 15:31:31 +0200

    modificato x db server scuola

--- core/config.php
+++ core/config.php
@@ -1,11 +1,11 @@
 <?php
 
-	$host = "localhost";
-	$user = "root";
-	$password = "";
+	$host = "10.40.10.100";
+	$user = "xxx";
+	$password = "xx";
 	$db = "gpoi_work";
 	
-	$conn = new mysqli($host, $user, $password, $db) or die("Errore connessione: ".mysqli_connect_errno());
+	$conn = new mysqli($host, $user, $password, $db) or die("Errore connessione: ".mysqli_connect_errno()); 
 	 
 	/*$host = "localhost";
 	$user = "root";
